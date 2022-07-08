05c399de2da12ba0be964639c2e089dbc420656f
Alex Koenigsberger <ak826@cornell.edu>
2022-07-07 19:15:05 -0500

    more CM page tabs and navbar

--- src/typing/types.tsx
+++ src/typing/types.tsx
@@ -3,9 +3,25 @@
 //---------------------------------------
 
 // CM Page Playing Results
-export interface PlayingStatsResult {
+export interface SongSearchResultLite {
     available: string,
     you: number,
     sheet: string,
     song: string
+};
+
+export interface SongSearchResult {
+    available: string,
+    played: number,
+    you: number,
+    sheet: string,
+    song: string,
+    composer: string,
+    arranger: string,
+    genre: string,
+    requests: number,
+    keysig: string,
+    timesig: string,
+    tempo: string,
+    added: string
 };
