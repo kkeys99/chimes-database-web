4de72aef1bc8c24e8790c15c5e4290bcb56a1244
Alex Koenigsberger <ak826@cornell.edu>
2022-12-04 13:02:18 -0600

    added column sort buttons, works but needs improevement

--- src/components/ResultsTable.tsx
+++ src/components/ResultsTable.tsx
@@ -8,9 +8,62 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import IconButton from '@mui/material/IconButton';
+import SvgIcon from "@mui/material/SvgIcon";
+import ButtonGroup from '@mui/material/ButtonGroup';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 import { resultTableRowData, Song } from '../typing/types';
 import {useState} from 'react'
+
+/*****************************************************************************/
+
+const playingStatsFields = ["Available", "You", "Sheet", "Song"];
+const playingStatsFieldsFull = ["Available", "Played", "You", "Sheet", "Song", "Composer", 
+                        "Arranger", "Genre", "Requests", "Key Sig", "Time Sig", "Tempo", "Added"];
+
+
+function getField(field: string) : string {
+    return field == "song" ?
+        "title" : field.replaceAll(" ", "");
+}
+
+// Example from https://mui.com/material-ui/react-table/
+// An ascending comparison function
+function ascendingCompare<T>(a: T, b: T, orderBy: keyof T){
+
+    if (a[orderBy] < b[orderBy]) {
+        console.log(-1)
+        return -1;
+    }
+    else if (a[orderBy] > b[orderBy]) {
+        console.log(1)
+        return 1;
+    }
+    else {
+        console.log(0)
+        return 0;
+    }
+}
+
+type Order = 'asc' | 'desc';
+
+// This function gets the compare *function* to use, whether ascend or descend.
+// It takes in the order - asc or desc - and the key by which to sort
+// Its return type a function that takes in 2 array items and returns a number
+function getCompareFunction( 
+    order: string, orderBy: any
+) : (
+    a: resultTableRowData,
+    b: resultTableRowData
+) => number {
+    return order == 'asc' ?
+        (a, b) => ascendingCompare(a, b, orderBy) :
+        (a, b) => -ascendingCompare(a, b, orderBy)
+    ;
+}
+
 
 /*****************************************************************************
  * FormattedCell
@@ -30,47 +83,28 @@ const FormattedCell = (props: {text: any}) => {
     )
 }
 
-const playingStatsFields = ["Available", "You", "Sheet", "Song"];
-const playingStatsFieldsFull = ["Available", "Played", "You", "Sheet", "Song", "Composer", 
-                        "Arranger", "Genre", "Requests", "Key Sig", "Time Sig", "Tempo", "Added"];
+/*****************************************************************************
+ * SortButton UNUSED
+ * 
+ * Description:
+ *   Table cell with formatting so we don't have to repeat this formatting
+ *   twice in the ResultsTable
+*****************************************************************************/
+type Arrows = typeof ArrowDropUpIcon | typeof ArrowDropDownIcon;
+type Direction = "up" | "down";
 
-
-// Example from https://mui.com/material-ui/react-table/
-
-// An ascending comparison function
-function ascendingCompare<T>(a: T, b: T, orderBy: keyof T){
-    if (a[orderBy] < b[orderBy]) {
-        console.log(-1)
-        return -1;
-    }
-    else if (a[orderBy] > b[orderBy]) {
-        console.log(1)
-        return 1;
-    }
-    else {
-        console.log(0)
-        return 0;
-    }
+const SortButton = (props: {component: Arrows, direction: Direction}) => {
+    
+    return (
+        <IconButton
+            sx={{p:0, height:12, width:12}}
+            disableRipple
+            // How to pass onClick here ??
+        >
+            <SvgIcon sx={{p:0, height:12, width:12}} component={props.component} viewBox={"6 6 12 12"} />
+        </IconButton>
+    )
 }
-
-type Order = 'asc' | 'desc'
-
-// This function gets the compare *function* to use, whether ascend or descend.
-// It takes in the order - asc or desc - and the key by which to sort
-// Its return type a function that takes in 2 array items and returns a number
-function getCompareFunction( 
-    order: Order, orderBy: any
-) : (
-    a: resultTableRowData,
-    b: resultTableRowData
-) => number {
-    return order == 'asc' ?
-        (a, b) => ascendingCompare(a, b, orderBy) :
-        (a, b) => -ascendingCompare(a, b, orderBy)
-    ;
-}
-
-
 
 /*****************************************************************************
  * ResultsTable
@@ -82,9 +116,17 @@ function getCompareFunction(
 const ResultsTable = (props: {data: resultTableRowData[], lite: boolean}) => {
    // Consider using mui styled() to style the table rows to alternate colors and hide borders
  
+    // State variables for sorting. Sorts the whole table by column specified by orderBy
     const [orderBy, setOrderBy] = useState("sheet");
+    const [sortDirection, setSortDirection] = useState("asc");
 
     const headerFields : string[] = (props.lite) ? playingStatsFields : playingStatsFieldsFull;
+
+    // Pass this into the Sort buttons to update sorting State variables
+    const sortButtonOnClick = (field: string, order: string)=> { 
+        setOrderBy(getField(field.toLowerCase()));
+        setSortDirection(order);
+    }
 
     return(
         <TableContainer >
@@ -93,16 +135,39 @@ const ResultsTable = (props: {data: resultTableRowData[], lite: boolean}) => {
                     <TableRow>
                         {headerFields.map((field) =>
                             <TableCell align='center'>
-                                <Typography color="primary.dark" variant="h2" fontWeight="bold">
-                                    {field}
-                                </Typography>
+                                <Stack direction="row">
+                                    <Typography 
+                                        color="primary.dark" variant="h2" fontWeight="bold"
+                                        sx={{pr:2}} // Pad to the right to space out buttons because button padding is weird
+                                    >
+                                        {field}
+                                    </Typography>
+                                    {/* Sorting Buttons. */}
+                                    {/* Tried to modularize this, but couldn't figure out best way to pass onClick properly */}
+                                    <ButtonGroup orientation="vertical">
+                                        <IconButton
+                                            sx={{p:0, height:12, width:12}}
+                                            disableRipple
+                                            onClick={ ()=> { sortButtonOnClick(field, "asc") } }
+                                        >
+                                            <SvgIcon sx={{p:0, height:12, width:12}} component={ArrowDropUpIcon} viewBox={"6 6 12 12"} />
+                                        </IconButton>
+                                        <IconButton
+                                            sx={{p:0, height:12, width:12}}
+                                            disableRipple
+                                            onClick={ ()=> { sortButtonOnClick(field, "desc") } }
+                                        >
+                                            <SvgIcon sx={{p:0, height:12, width:12}} component={ArrowDropDownIcon} viewBox={"6 6 12 12"} />
+                                        </IconButton>
+                                    </ButtonGroup>
+                                </Stack>
                             </TableCell>
                         )}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {props.data &&
-                    props.data.sort(getCompareFunction('asc', orderBy))
+                    {props.data && // Only try to render body contents if data exists
+                    props.data.sort(getCompareFunction(sortDirection, orderBy))
                     .map((rowData, index) => 
                         <TableRow 
                             sx={{
