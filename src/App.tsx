8a09bd566ba9b8e9787d1aa3cae1ff32b7ef8968
Alex Koenigsberger <52012904+alexk826@users.noreply.github.com>
2023-02-28 18:29:34 -0600

    incorporate songpage w links and fakeDB fetches (#8)
    * incorporate songpage w links and fakeDB fetches
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SiteHeader from "./components/SiteHeader";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CM from "./pages/CM";
 import Home from "./pages/Home";
+import SongPage from "./components/SongPage";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/CMs/:initials" element={<CM />} />
+          <Route path="/song/:id" element={<SongPage />} />
         </Routes>
       </Router>
     </ThemeProvider>
